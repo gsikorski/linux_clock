53124eccb0b6c2e3eaaa9825866bce9dfe7ed1d5
Greg <sikorski.grzegorz@gmail.com>
2023-10-28 15:45:40 +0200

    Update linux_clock.hpp

--- linux_clock.hpp
+++ linux_clock.hpp
@@ -5,13 +5,13 @@
 
 namespace linux_clock {
 
-template <clockid_t ClockId>
+template <clockid_t ClockId, bool IsSteady = true>
 struct linux_clock {
     using duration = typename std::chrono::nanoseconds;
     using rep = typename duration::rep;
     using period = typename duration::period;
     using time_point = typename std::chrono::time_point<linux_clock<ClockId>>;
-    static constexpr bool is_steady = true;
+    static constexpr bool is_steady = IsSteady;
     static auto now() noexcept -> time_point {
         auto current_time = timespec {};
         if(0 != clock_gettime(ClockId, &current_time)) { return time_point {}; }
